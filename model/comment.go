713f4586b6b249493fefcf42063d814377214fd4
mythsman <mythsman@foxmail.com>
2022-10-25 17:26:14 +0800

    fix

--- model/comment.go
+++ model/comment.go
@@ -6,14 +6,14 @@ import (
 
 type Comment struct {
 	ID        uint64
-	DoubanUid uint64 `gorm:"not null;uniqueIndex:uk_comment,index:idx_comment,priority:1"`
-	DoubanId  uint64 `gorm:"not null;uniqueIndex:uk_comment,index:idx_comment,priority:2"`
-	Type      uint8  `gorm:"not null;uniqueIndex:uk_comment,index:idx_comment,priority:3"`
+	DoubanUid uint64 `gorm:"not null;uniqueIndex:uk_comment;index:idx_comment;priority:1"`
+	DoubanId  uint64 `gorm:"not null;uniqueIndex:uk_comment;index:idx_comment;priority:2"`
+	Type      uint8  `gorm:"not null;uniqueIndex:uk_comment;index:idx_comment;priority:3"`
 	Rate      uint8
 	Label     string    `gorm:"type:varchar(512)"`
 	Comment   string    `gorm:"type:mediumtext"`
-	Status    uint8     `gorm:"not null;index:idx_comment,priority:4"`
-	MarkDate  time.Time `gorm:"not null;type:datetime(3);index:idx_comment,priority:5"`
+	Status    uint8     `gorm:"not null;index:idx_comment;priority:4"`
+	MarkDate  time.Time `gorm:"not null;index:idx_comment;priority:5"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
