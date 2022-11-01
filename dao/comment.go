4075d828ecc1b693facd8ba8761ef14c304a0964
mythsman <mythsman@foxmail.com>
2022-11-01 19:31:46 +0800

    add comment dao

--- dao/comment.go
+++ dao/comment.go
@@ -11,3 +11,15 @@ func UpsertComment(comment *model.Comment) {
 		common.Db.Create(comment)
 	}
 }
+
+func GetComment(doubanId uint64, doubanUid uint64, t uint8) *model.Comment {
+	comment := &model.Comment{}
+	common.Db.Where("douban_id = ? AND douban_uid = ? AND type = ?", doubanId, doubanUid, t).Find(comment)
+	return comment
+}
+
+func ListComment(doubanIds *[]uint64, doubanUid uint64, t uint8) *[]model.Comment {
+	var comment *[]model.Comment
+	common.Db.Where("douban_id IN ? AND douban_uid = ? type = ?", *doubanIds, doubanUid, t).Find(&comment)
+	return comment
+}
