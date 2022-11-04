6cfbffb36281c2985aed4c59c764a5501b1eba47
mythsman <mythsman@foxmail.com>
2022-11-04 16:06:25 +0800

    add permission

--- controller/admin.go
+++ controller/admin.go
@@ -2,15 +2,22 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"mouban/consts"
 	"mouban/logic"
 	"strconv"
 )
 
 func GetOverview(ctx *gin.Context) {
+	if !checkPermission(ctx) {
+		return
+	}
 }
 
 func CrawlUser(ctx *gin.Context) {
+	if !checkPermission(ctx) {
+		return
+	}
 	id := ctx.Query("id")
 	idInt, err := strconv.ParseUint(id, 10, 64)
 	if err != nil {
@@ -21,6 +28,9 @@ func CrawlUser(ctx *gin.Context) {
 }
 
 func CrawlItem(ctx *gin.Context, t uint8) {
+	if !checkPermission(ctx) {
+		return
+	}
 	id := ctx.Query("id")
 	idInt, err := strconv.ParseUint(id, 10, 64)
 	if err != nil {
@@ -28,4 +38,13 @@ func CrawlItem(ctx *gin.Context, t uint8) {
 		return
 	}
 	logic.Dispatch(idInt, t)
+}
+
+func checkPermission(ctx *gin.Context) bool {
+	token := ctx.Query("token")
+	if token != viper.GetString("admin.token") {
+		BizError(ctx, "参数错误")
+		return false
+	}
+	return true
 }
