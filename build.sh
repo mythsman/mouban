2a445a62656ca209a52c21c7d8ccd0eb7362060c
mythsman <mythsman@foxmail.com>
2022-11-05 22:50:40 +0800

    divide server and agent

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build server.go
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build agent.go
