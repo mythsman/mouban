71f4c4a17c429d83c2238ceeaf583c73b3919007
mythsman <mythsman@foxmail.com>
2022-11-17 17:25:01 +0800

    support auto push

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,13 @@
-FROM golang:1.17 as build
-
-ENV GO111MODULE=on
-ENV GOPROXY=https://goproxy.cn,direct
-
-WORKDIR /srv
-
-ADD . .
-
-RUN GOOS=linux CGO_ENABLED=0 GOARCH=arm64 go build -ldflags="-s -w" -installsuffix cgo -o main main.go
-
-FROM scratch as prod
+FROM alpine
 
 WORKDIR /srv
 ENV TZ=Asia/Shanghai
 
-COPY --from=build /srv/main /srv
-COPY --from=build /srv/application.yml.sample /srv/application.yml
-COPY --from=build /srv/cookie.txt.sample /srv/cookie.txt
-COPY --from=build /usr/share/zoneinfo /usr/share/zoneinfo
-COPY --from=build /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
+RUN apk add --no-cache tzdata
+
+COPY main /srv
+COPY application.yml.sample /srv/application.yml
+COPY cookie.txt.sample /srv/cookie.txt
 
 EXPOSE 8080
 
---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+GOOS=linux CGO_ENABLED=0 GOARCH=amd64 go build -ldflags="-s -w" -installsuffix cgo -o main main.go
+
+docker buildx build -t mythsman/mouban:amd64-latest -f  Dockerfile --platform=linux/amd64 .
+
+rm -rf main
+
+GOOS=linux CGO_ENABLED=0 GOARCH=arm64 go build -ldflags="-s -w" -installsuffix cgo -o main main.go
+
+docker buildx build -t mythsman/mouban:arm64-latest -f  Dockerfile --platform=linux/arm64 .
+
+rm -rf main
+
+docker manifest annotate mythsman/mouban  mythsman/mouban:amd64-latest --os linux --arch amd64
+
+docker manifest annotate mythsman/mouban  mythsman/mouban:arm64-latest --os linux --arch arm64 --variant v8
+
+docker push mythsman/mouban:amd64-latest
+
+docker push mythsman/mouban:arm64-latest
+
+docker manifest rm mythsman/mouban
+
+docker manifest create mythsman/mouban mythsman/mouban:arm64-latest mythsman/mouban:amd64-latest
+
+docker manifest push mythsman/mouban
