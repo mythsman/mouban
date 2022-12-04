e13b5e65e470118dfcefe76489ad0862ddbe82a8
mythsman <mythsman@foxmail.com>
2022-12-04 21:31:01 +0800

    add index

--- model/schedule.go
+++ model/schedule.go
@@ -7,11 +7,11 @@ import (
 type Schedule struct {
 	ID        uint64
 	DoubanId  uint64 `gorm:"not null;uniqueIndex:uk_schedule;"`
-	Type      uint8  `gorm:"not null;uniqueIndex:uk_schedule;index:idx_status;index:idx_result;priority=1"`
-	Status    uint8  `gorm:"not null;index:idx_status;priority=2"`
-	Result    uint8  `gorm:"not null;index:idx_result;priority=2"`
+	Type      uint8  `gorm:"not null;uniqueIndex:uk_schedule;index:idx_status;index:idx_result;index:idx_search;priority=1"`
+	Status    uint8  `gorm:"not null;index:idx_status;index:idx_search;priority=2"`
+	Result    uint8  `gorm:"not null;index:idx_result;index:idx_search;priority=2"`
 	CreatedAt time.Time
-	UpdatedAt time.Time `gorm:"index:idx_result;index:idx_status;priority=3"`
+	UpdatedAt time.Time `gorm:"index:idx_result;index:idx_status;index:idx_search;priority=3"`
 }
 
 func (Schedule) TableName() string {
