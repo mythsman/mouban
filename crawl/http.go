730526f4bd72d0ab50796192b0b4c5c5e2c27fac
mythsman <mythsman@foxmail.com>
2022-12-04 10:58:16 +0800

    fix

--- crawl/http.go
+++ crawl/http.go
@@ -23,7 +23,7 @@ var userAgent = []string{
 	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/106.0.1370.42",
 	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/106.0.1370.47",
 	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/106.0.1370.52",
-}[rand.Intn(13)]
+}[rand.Intn(7)]
 
 var client http.Client
 var UserLimiter *rate.Limiter
