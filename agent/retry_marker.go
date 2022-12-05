f75a4ebfbeca0f9f9ae043f7cf1c00c8d3608744
mythsman <mythsman@foxmail.com>
2022-12-05 11:26:30 +0800

    optimize retry

--- agent/retry_marker.go
+++ agent/retry_marker.go
@@ -16,13 +16,58 @@ func runRetry() {
 		}
 		time.Sleep(time.Second * 5)
 	}()
-	schedule := dao.SearchSchedule(consts.ScheduleStatusCrawled, consts.ScheduleResultUnready)
-	if schedule != nil {
-		changed := dao.CasScheduleStatus(schedule.DoubanId, schedule.Type, consts.ScheduleStatusToCrawl, consts.ScheduleStatusCrawled)
-		if changed {
-			log.Printf("process retry %d %d \n", schedule.Type, schedule.DoubanId)
+
+	pendingBook := dao.SearchScheduleByStatus(consts.TypeBook, consts.ScheduleStatusToCrawl)
+	if pendingBook == nil {
+		retryBook := dao.SearchScheduleByAll(consts.TypeBook, consts.ScheduleStatusCrawled, consts.ScheduleResultUnready)
+		if retryBook != nil {
+			changed := dao.CasScheduleStatus(retryBook.DoubanId, retryBook.Type, consts.ScheduleStatusToCrawl, consts.ScheduleStatusCrawled)
+			if changed {
+				log.Printf("process retry book %d %d\n", retryBook.Type, retryBook.DoubanId)
+			}
 		}
 	}
+
+	pendingMovie := dao.SearchScheduleByStatus(consts.TypeMovie, consts.ScheduleStatusToCrawl)
+	if pendingMovie == nil {
+		retryMovie := dao.SearchScheduleByAll(consts.TypeMovie, consts.ScheduleStatusCrawled, consts.ScheduleResultUnready)
+		if retryMovie != nil {
+			changed := dao.CasScheduleStatus(retryMovie.DoubanId, retryMovie.Type, consts.ScheduleStatusToCrawl, consts.ScheduleStatusCrawled)
+			if changed {
+				log.Printf("process retry movie %d %d\n", retryMovie.Type, retryMovie.DoubanId)
+			}
+		}
+	}
+	pendingGame := dao.SearchScheduleByStatus(consts.TypeGame, consts.ScheduleStatusToCrawl)
+	if pendingGame == nil {
+		retryGame := dao.SearchScheduleByAll(consts.TypeGame, consts.ScheduleStatusCrawled, consts.ScheduleResultUnready)
+		if retryGame != nil {
+			changed := dao.CasScheduleStatus(retryGame.DoubanId, retryGame.Type, consts.ScheduleStatusToCrawl, consts.ScheduleStatusCrawled)
+			if changed {
+				log.Printf("process retry game %d %d\n", retryGame.Type, retryGame.DoubanId)
+			}
+		}
+	}
+
+	pendingUser := dao.SearchScheduleByStatus(consts.TypeUser, consts.ScheduleStatusToCrawl)
+	if pendingUser == nil {
+		retryUser := dao.SearchScheduleByAll(consts.TypeUser, consts.ScheduleStatusCrawled, consts.ScheduleResultUnready)
+		if retryUser != nil {
+			changed := dao.CasScheduleStatus(retryUser.DoubanId, retryUser.Type, consts.ScheduleStatusToCrawl, consts.ScheduleStatusCrawled)
+			if changed {
+				log.Printf("process retry user %d %d\n", retryUser.Type, retryUser.DoubanId)
+			}
+		} else {
+			discoverUser := dao.SearchScheduleByStatus(consts.TypeUser, consts.ScheduleStatusCanCrawl)
+			if discoverUser != nil {
+				changed := dao.CasScheduleStatus(discoverUser.DoubanId, consts.TypeUser, consts.ScheduleStatusToCrawl, consts.ScheduleStatusCanCrawl)
+				if changed {
+					log.Printf("process discover user %d\n", discoverUser.DoubanId)
+				}
+			}
+		}
+	}
+
 }
 func init() {
 	if viper.GetString("agent.enable") != "true" {
