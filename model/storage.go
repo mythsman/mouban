b80a92a6a1c613f98fe942a78c934071dc3bf516
mythsman <mythsman@foxmail.com>
2023-07-15 14:55:15 +0800

    fix

--- model/storage.go
+++ model/storage.go
@@ -6,9 +6,9 @@ import (
 
 type Storage struct {
 	ID        uint64
-	Source    string `gorm:"not null;uniqueIndex;"`
-	Target    string `gorm:"not null;index;"`
-	Md5       string `gorm:"not null;index;"`
+	Source    string `gorm:"type:varchar(256);not null;uniqueIndex;"`
+	Target    string `gorm:"type:varchar(256);not null;index;"`
+	Md5       string `gorm:"type:varchar(64);not null;index;"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
