b937fe5e24b6c7cdc92383704a63770a420a1f20
mythsman <mythsman@foxmail.com>
2026-06-24 05:40:20 +0000

    fix

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 GOOS=linux CGO_ENABLED=0 GOARCH=amd64 go build -ldflags="-s -w" -o main main.go
 
-sudo docker build -t mythsman/mouban -f Dockerfile --platform=linux/amd64 .
+#sudo docker build -t mythsman/mouban -f Dockerfile --platform=linux/amd64 .
 
-rm -rf main
+#rm -rf main
 
-sudo docker push mythsman/mouban
+#sudo docker push mythsman/mouban
