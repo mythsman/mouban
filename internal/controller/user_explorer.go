b8b46c55be41dd341632050da71777f5e31f4173
mythsman <mythsman@foxmail.com>
2026-06-25 18:33:17 +0000

    feat: refine SSR explorer UI with tabs and clearer labels

--- internal/controller/user_explorer.go
+++ internal/controller/user_explorer.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 
 	"mouban/internal/consts"
 	"mouban/internal/dao"
@@ -13,19 +14,32 @@ import (
 )
 
 type UserTypeSection struct {
-	Key     string
-	Name    string
-	Wish    []model.CommentVO
-	Do      []model.CommentVO
-	Collect []model.CommentVO
-	Total   int
+	Key          string
+	Name         string
+	WishLabel    string
+	DoLabel      string
+	CollectLabel string
+	Wish         []model.CommentVO
+	Do           []model.CommentVO
+	Collect      []model.CommentVO
+	Total        int
+}
+
+type UserProfileView struct {
+	ID            uint64
+	Name          string
+	Domain        string
+	Thumbnail     string
+	PublishAtText string
+	SyncAtText    string
+	CheckAtText   string
 }
 
 type UserExplorerPageData struct {
 	Query      string
 	SelectedID string
 	Candidates []ResolveUserVO
-	User       *model.UserVO
+	User       *UserProfileView
 	Sections   []UserTypeSection
 	Error      string
 }
@@ -43,7 +57,7 @@ func UserExplorerPage(ctx *gin.Context) {
 
 	loggedUserID := uint64(0)
 
-	if q != "" {
+	if id == "" && q != "" {
 		data.Candidates = resolveUsers(q)
 	}
 
@@ -57,12 +71,21 @@ func UserExplorerPage(ctx *gin.Context) {
 			if user == nil {
 				data.Error = "用户不存在或尚未收录"
 			} else {
-				data.User = user.Show()
+				vo := user.Show()
+				data.User = &UserProfileView{
+					ID:            vo.ID,
+					Name:          vo.Name,
+					Domain:        vo.Domain,
+					Thumbnail:     vo.Thumbnail,
+					PublishAtText: formatUnixCN(vo.PublishAt),
+					SyncAtText:    formatUnixCN(vo.SyncAt),
+					CheckAtText:   formatUnixCN(vo.CheckAt),
+				}
 				data.Sections = []UserTypeSection{
-					buildUserTypeSection(doubanUid, consts.TypeBook.Code, "book", "图书"),
-					buildUserTypeSection(doubanUid, consts.TypeMovie.Code, "movie", "电影"),
-					buildUserTypeSection(doubanUid, consts.TypeGame.Code, "game", "游戏"),
-					buildUserTypeSection(doubanUid, consts.TypeSong.Code, "song", "音乐"),
+					buildUserTypeSection(doubanUid, consts.TypeBook.Code, "book", "图书", "想读", "在读", "读过"),
+					buildUserTypeSection(doubanUid, consts.TypeMovie.Code, "movie", "电影", "想看", "在看", "看过"),
+					buildUserTypeSection(doubanUid, consts.TypeGame.Code, "game", "游戏", "想玩", "在玩", "玩过"),
+					buildUserTypeSection(doubanUid, consts.TypeSong.Code, "song", "音乐", "想听", "在听", "听过"),
 				}
 			}
 		}
@@ -72,17 +95,27 @@ func UserExplorerPage(ctx *gin.Context) {
 	ctx.HTML(http.StatusOK, "user_explorer.tmpl", data)
 }
 
-func buildUserTypeSection(doubanUid uint64, t uint8, key string, name string) UserTypeSection {
+func buildUserTypeSection(doubanUid uint64, t uint8, key string, name string, wishLabel string, doLabel string, collectLabel string) UserTypeSection {
 	wish := buildUserCommentsVO(doubanUid, t, consts.ActionWish.Code, 0)
 	doItems := buildUserCommentsVO(doubanUid, t, consts.ActionDo.Code, 0)
 	collect := buildUserCommentsVO(doubanUid, t, consts.ActionCollect.Code, 0)
 
 	return UserTypeSection{
-		Key:     key,
-		Name:    name,
-		Wish:    wish,
-		Do:      doItems,
-		Collect: collect,
-		Total:   len(wish) + len(doItems) + len(collect),
+		Key:          key,
+		Name:         name,
+		WishLabel:    wishLabel,
+		DoLabel:      doLabel,
+		CollectLabel: collectLabel,
+		Wish:         wish,
+		Do:           doItems,
+		Collect:      collect,
+		Total:        len(wish) + len(doItems) + len(collect),
 	}
+}
+
+func formatUnixCN(ts int64) string {
+	if ts <= 0 {
+		return "暂无"
+	}
+	return time.Unix(ts, 0).In(time.Local).Format("2006-01-02 15:04:05")
 }
